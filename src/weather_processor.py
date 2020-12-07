c509abb8d824451bcf08a30eef2db0cf522e0f50
Longxun Jin(Tommy) <ljin38@aceproject.space>
2020-12-07 03:08:31 -0600

    user chose to download full set weather data

--- src/weather_processor.py
+++ src/weather_processor.py
@@ -16,76 +16,19 @@ class WeatherProcessor:
     or a specific month from a specific year.
     """
 
-    def __init__(self):
-        self.database_choice = ''
-        self.plot_choice = ''
-
-    def download_or_update_database(self):
-        """
-        this class is to allow user to download or update their database
-        :return: print the message to show the user if the data base has been downloaded or updated
-        """
-        self.database_choice = input('please input. "D" or "d" to download DB, "U"or "u" to update DB,')
-        while not (self.database_choice.lower() == 'd' or self.database_choice.lower() == 'u'):
-            self.database_choice = input('please input. "d" or "D" to download DB, "u"or "U" to update DB')
-
+    @staticmethod
+    def download_or_update_database():
+        user_choice = input('please select a choice to input. "d" or "D" to download a full set of '
+                            'weather data, "u"or "U" to update the weather data according to the current date')
         iso_time_format = '%Y-%m-%d'
         current_time = datetime.datetime.now().strftime(iso_time_format)
-
-        if self.database_choice.lower() == 'd':
+        if user_choice.lower() =='d':
             my_scraper = WeatherScraper()
-            years = range(1996, int(current_time[:4]) + 1)
+            years = range(1996, current_time[:4]+1)
             for year in years:
                 my_scraper.start_scraping('', year)
             mydb = DBOperations('weather.sqlite')
             mydb.initialize_db()
             mydb.purge_data()
             mydb.save_data(my_scraper.weather)
-            print('your database has been downloaded!')
-            self.plot_choice = input('please input "B" or "b" to draw box plots for a year range,'
-                                     '"L" or "l" to draw a line plot for a selected month and year')
-            if self.plot_choice.lower() == 'b':
-                self.box_plot()
 
-            elif self.plot_choice.lower() == 'l':
-                self.line_plot()
-
-        elif self.database_choice.lower() == 'u':
-            my_scraper = WeatherScraper()
-            db_latest_date = ''  # 数据库里最新信息 怎么get????????
-            years = range(db_latest_date[:4], current_time[:4] + 1)
-            for year in years:
-                my_scraper.start_scraping('', year)
-            mydb = DBOperations('weather.sqlite')
-            mydb.initialize_db()
-            mydb.purge_data()
-            mydb.save_data(my_scraper.weather)
-            print('your database has been updated!')
-            self.plot_choice = input('please input "B" or "b" to draw box plots for a year range,'
-                                     '"L" or "l" to draw a line plot for a selected month and year')
-            if self.plot_choice.lower() == 'b':
-                self.box_plot()
-
-            elif self.plot_choice.lower() == 'l':
-                self.line_plot()
-
-    def box_plot(self):
-        print("please input two separated years to generate a year range.(example: 2015 2018)")
-        years = list(map(int, input("please input now: ").split()))
-        my_plot = PlotOperations()
-        print(my_plot.generate_box_plot(years[0], years[1]))
-        print('the mean temperature from ' + str(years[0]) + 'to' + str(
-            years[1]) + 'has been plotted')
-
-    def line_plot(self):
-        print("please input a specific month followed by a specific year,"
-              " to generate a year range.(example: 3 2018)")
-        nums = list(map(int, input("please input now: ").split()))
-        my_plot = PlotOperations()
-        print(my_plot.generate_line_plot(nums[0], nums[1]))
-        print('the mean temperature from ' + str(nums[0]) + 'to' + str(nums[1]) + 'has been plotted')
-
-
-if __name__ == '__main__':
-    my_weather_processor = WeatherProcessor()
-    my_weather_processor.download_or_update_database()
