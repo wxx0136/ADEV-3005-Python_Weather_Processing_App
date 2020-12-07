5bdc8631c3322d6525ab4bee8e649fe03ef67874
Longxun Jin(Tommy) <ljin38@aceproject.space>
2020-12-06 23:24:00 -0600

    changed data format according to the format fetched from database

--- src/plot_operations.py
+++ src/plot_operations.py
@@ -1,3 +1,7 @@
+"""
+this module plots the weather data from data fetched from database
+include
+"""
 from requests import get
 import matplotlib.pyplot as plt
 from dateutil import parser
@@ -11,22 +15,35 @@ class PlotOperations:
     plot weather details
     """
 
-    # weather_data[0].append(my_dict[calendar.xxx]['mean'])
-    # my_dict = {
-    #     '2020-12-01': {'Max': '1.3', 'Min': '-9.4', 'Mean': '-4.0'},
-    #     '2020-12-02': {'Max': '4.0', 'Min': '-6.8', 'Mean': '-1.4'},
-    #     '2020-12-03': {'Max': '3.3', 'Min': '-8.4', 'Mean': '-2.5'},
-    #     '2020-12-04': {'Max': '1.6', 'Min': '-10.6', 'Mean': '-4.5'}
-    #     }
+    def __init__(self):
+        self.box_plot_path_saving_dict = {}
+        self.line_plot_path_saving_dict = {}
 
-    # my_list = [
+    # def get_box_plot_saving_path(self, start_year: int, end_year: int) -> dict:
+    #     """
+    #     :param start_year: the year started
+    #     :param end_year: the year ended
+    #     :return: returns a dictionary of box plot saving paths
+    #     """
+    #     for year in range(start_year, end_year+1):
+    #         self.box_plot_path_saving_dict[str(year)] = self.generate_box_plot(start_year,end_year)
+    #     return self.box_plot_path_saving_dict
+
+    # fetched_list_format = [
     #     (366, '2020-01-01', 'StationID=27174', -12.3, -3.5, -7.9),
     #     (367, '2020-01-02', 'StationID=27174', -8.2, -6.0, -7.1),
     #     (368, '2020-01-03', 'StationID=27174', -6.8, -1.0, -3.9)
     # ]
-    @staticmethod
-    def generate_box_plot(start_year: int, end_year: int):
-        my_scraper = WeatherScraper()
+
+    def generate_box_plot(self, start_year: int, end_year: int) -> str:
+        """
+        :param end_year: starting year for box plotting
+        :param start_year: ending year for line plotting
+        :return: returns the generated box plot pictures' saving paths class instance
+        """
+
+        my_db = DBOperations('weather.sqlite')
+
         years = range(start_year, end_year + 1)
         years_weather_data = {}
 
@@ -35,12 +52,12 @@ class PlotOperations:
             yearly_data = []
             current_year = ''
             for month in months:
-                monthly_dict = my_scraper.get_weather_dict(year, month)
+                monthly_list = my_db.fetch_data(year, month)
                 monthly_mean_temp = []
-                for key, value in monthly_dict.items():
-                    if is_number(value['Mean']):
-                        monthly_mean_temp.append(float(value['Mean']))
-                        current_year = key[:4]
+                for item in monthly_list:
+                    if is_number(item[5]):
+                        monthly_mean_temp.append(float(item['Mean']))
+                        current_year = item[1][:4]
                 yearly_data.append(monthly_mean_temp)
             years_weather_data.update({current_year: yearly_data})
         # format: {2019:[[Jan],2:[Feb],..,12:[Dec]], 2020: [[Jan],...12:[Dec]]}
@@ -52,30 +69,57 @@ class PlotOperations:
             plt.xlabel('Month')
             plt.ylabel('Temperature (Celsius)')
             plt.title(plot_title)
+            save_path = './pictures/' + str(key) + '.jpg'
+            plt.savefig(save_path)
+            self.box_plot_path_saving_dict[str(key)] = save_path
             plt.show()
+
+        return self.box_plot_path_saving_dict
 
         # labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-    @staticmethod
-    def generate_line_plot(specific_month: int, specific_year: int):
-        my_scraper = WeatherScraper()
+    def generate_line_plot(self, specific_month: int, specific_year: int):
+        """
+        :param specific_month: the chosen month for line plotting
+        :param specific_year: the chosen year for line plotting
+        :return: returns the generated line plot pictures' saving paths class instance
+        """
+        my_db = DBOperations('weather.sqlite')
         specific_month_data = []
         specific_timestamp = []
-        monthly_dict = my_scraper.get_weather_dict(specific_year, specific_month)
-        for k, v in monthly_dict.items():
-            if is_number(v['Mean']):
-                specific_month_data.append(float(v['Mean']))
-                specific_timestamp.append(float(k[-2:]))
+        monthly_list = my_db.fetch_data(specific_year, specific_month)
+        for item in monthly_list:
+            if is_number(item[5]):
+                specific_month_data.append(float(item[5]))
+                specific_timestamp.append(float(item[1][-2:]))
         print(specific_year, '-', specific_month, ':', specific_month_data)
 
         plt.plot(specific_timestamp, specific_month_data)
         plt.xlabel('Day')
         plt.ylabel('Temperature (Celsius)')
-        plot_title = 'Daily Temperature Distribution for:' + str(specific_month) + ' in ' + str(specific_year)
+        plot_title = 'Daily Temperature Distribution for:' + str(specific_year) + ' - ' + str(specific_month)
         plt.title(plot_title)
+        save_path = './pictures/' + str(specific_year) + '-' + str(specific_month) + '.jpg'
+        plt.savefig(save_path)
+        self.line_plot_path_saving_dict[str(specific_year) + '-' + str(specific_month)] = save_path
         plt.show()
+
+        return self.line_plot_path_saving_dict
 
 
 if __name__ == '__main__':
-    PlotOperations.generate_box_plot(2018, 2019)
-    PlotOperations.generate_line_plot(3, 2018)
+    my_scraper = WeatherScraper()
+    my_scraper.start_scraping('', 2018)
+    my_scraper.start_scraping('', 2020)
+
+    mydb = DBOperations('weather.sqlite')
+    mydb.initialize_db()
+    mydb.purge_data()
+    mydb.save_data(my_scraper.weather)
+
+    my_plot = PlotOperations()
+    print(my_plot.generate_box_plot(2018, 2020))
+    print(my_plot.generate_line_plot(3, 2018))
+
+    # PlotOperations.generate_box_plot(2018, 2019)
+    # PlotOperations.generate_line_plot(3, 2018)
