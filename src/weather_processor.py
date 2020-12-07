76cb78e4e3dbc6517898c26ae381262809d7c7b9
Longxun Jin(Tommy) <ljin38@aceproject.space>
2020-12-07 04:11:32 -0600

    user chose to update

--- src/weather_processor.py
+++ src/weather_processor.py
@@ -18,17 +18,32 @@ class WeatherProcessor:
 
     @staticmethod
     def download_or_update_database():
+        """
+
+        :return:
+        """
         user_choice = input('please select a choice to input. "d" or "D" to download a full set of '
                             'weather data, "u"or "U" to update the weather data according to the current date')
         iso_time_format = '%Y-%m-%d'
         current_time = datetime.datetime.now().strftime(iso_time_format)
-        if user_choice.lower() =='d':
+        if user_choice.lower() == 'd':
             my_scraper = WeatherScraper()
-            years = range(1996, current_time[:4]+1)
+            years = range(1996, current_time[:4] + 1)
             for year in years:
                 my_scraper.start_scraping('', year)
             mydb = DBOperations('weather.sqlite')
             mydb.initialize_db()
             mydb.purge_data()
             mydb.save_data(my_scraper.weather)
-
+        elif user_choice.lower() == 'u':
+            my_scraper = WeatherScraper()
+            db_latest_date = ''
+            years = range(db_latest_date[:4], current_time[:4] + 1)
+            for year in years:
+                my_scraper.start_scraping('', year)
+            mydb = DBOperations('weather.sqlite')
+            mydb.initialize_db()
+            mydb.purge_data()
+            mydb.save_data(my_scraper.weather)
+        else:
+            print("please make your choice from options.please select a choice to input. ")
