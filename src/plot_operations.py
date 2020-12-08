4114f76ce09adffb5543ffa640187d52fe03d319
wxx <wxx0136@gmail.com>
2020-12-07 20:05:22 -0600

    temp save

--- src/plot_operations.py
+++ src/plot_operations.py
@@ -4,12 +4,11 @@ include methods:
 generate_box_plot is to draw box plot from year to year
 generate_line_plot is to draw line plot from a specific month from a chosen year
 """
-from requests import get
 import matplotlib.pyplot as plt
-from dateutil import parser
+
+from common import is_number
 from db_operations import DBOperations
 from scrape_weather import WeatherScraper
-from common import is_number
 
 
 class PlotOperations:
@@ -22,12 +21,6 @@ class PlotOperations:
         self.box_plot_path_saving_dict = {}
         self.line_plot_path_saving_dict = {}
 
-    # fetched_list_format = [
-    #     (366, '2020-01-01', 'StationID=27174', -12.3, -3.5, -7.9),
-    #     (367, '2020-01-02', 'StationID=27174', -8.2, -6.0, -7.1),
-    #     (368, '2020-01-03', 'StationID=27174', -6.8, -1.0, -3.9)
-    # ]
-
     def generate_box_plot(self, start_year: int, end_year: int) -> dict:
         """
         :param end_year: starting year for box plotting
@@ -36,29 +29,24 @@ class PlotOperations:
         """
 
         my_db = DBOperations('weather.sqlite')
+        monthly_weather_data = {}  # format: [1:[Jan temps],2:[Feb temps],..,12:[Dec temps]]
 
-        years = range(start_year, end_year + 1)
-        monthly_weather_data = {}
-
-        for year in years:
-            months = range(1, 13)
-            yearly_data = []
-            for month in months:
+        for year in range(start_year, end_year + 1):
+            for month in range(1, 13):
                 monthly_list = my_db.fetch_data(year, month)
                 if month not in monthly_weather_data:
                     monthly_weather_data[month] = []
                 for item in monthly_list:
                     if is_number(item[5]):
                         monthly_weather_data[month].append(float(item[5]))
-        # format: [1:[Jan temps],2:[Feb temps],..,12:[Dec temps]]
-        plot_title = 'Monthly Temperature Distribution for:' + str(start_year) + ' to ' + str(end_year)
 
-        print(monthly_weather_data)
+        plot_title = 'Monthly Temperature Distribution for: ' + str(start_year) + ' to ' + str(end_year)
+        # print(monthly_weather_data)
         plt.boxplot(monthly_weather_data.values(), sym="o", whis=1.5)
         plt.xlabel('Month')
         plt.ylabel('Temperature (Celsius)')
         plt.title(plot_title)
-        save_path = './images/boxplot-'+str(start_year)+'_to_'+str(end_year)+'.jpg'
+        save_path = './images/boxplot-' + str(start_year) + '_to_' + str(end_year) + '.jpg'
         plt.savefig(save_path)
         self.box_plot_path_saving_dict[str(start_year) + '-' + str(end_year)] = save_path
         plt.show()
