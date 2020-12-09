5d6a04c42f746bcff2604a4752bd69c8e586a505
wxx <wxx0136@gmail.com>
2020-12-09 03:04:30 -0600

    Fixed the bug: If there is no data for today, it won't be scarped.

--- src/scrape_weather.py
+++ src/scrape_weather.py
@@ -99,8 +99,9 @@ class WeatherScraper(HTMLParser):
         daily_temps_list = []
         for item in result_grouping:
             if len(item) >= 3:
-                my_dict = {"Max": str(item[0]), "Min": str(item[1]), "Mean": str(item[2])}
-                daily_temps_list.append(my_dict)
+                if item[0] != '' and item[1] != '' and item[2] != '':
+                    my_dict = {"Max": str(item[0]), "Min": str(item[1]), "Mean": str(item[2])}
+                    daily_temps_list.append(my_dict)
 
         # print('debug: daily_temps_list')
         # for item in daily_temps_list:
